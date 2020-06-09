161838a5437eb07673781f312460c1eb75abea9b
lpl-code <54057612+lpl-code@users.noreply.github.com>
2020-06-09 19:33:21 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,5 +3,7 @@ package helloworld;
 import org.omg.Messaging.SyncScopeHelper;
 
 public class HelloWorld {
-	System.out.println("Hello World!");
+	public static void main(String[] args) {
+		System.out.println("Hello World! Ëæ±ã¸ÄÒ»¸Ä");
+	}
 }
